2da30d648158b6aa2df79907e2ad7a2217b3e74c
ahuang-cs <ahuang-cs@users.noreply.github.com>
2023-04-06 00:47:40 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ahuang-cs"
+original_name="cisc179"
+original_urlname="CISC179"
+original_description="Awesome cisc179 created by ahuang-cs"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv cisc179 $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from cisc179.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "cisc179"
